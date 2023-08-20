3485c6c056c75b60e8ef7e179da5ee4c979cdd4a
HKL <bastlwalzer@gmail.com>
2023-08-20 15:26:57 +0900

    Kafka Producer Test

--- README.md
+++ README.md
@@ -6,3 +6,17 @@
 * Redis를 이용한 해결 방법
    * Redis는 싱글 쓰레드 기반이라 Race Condition 을 해결 할 수 있을 뿐만 아니라 incr 방식을 사용하면 성능도 어느정도 커버 가능
    * Redis를 이용하였을 때 대규모 트래픽이 발생한다면 RDB의 CPU 사용량이 급증하여 성능저하 후 오류가 발생 할 수 있음. 
+* KafKa를 이용한 해결 방법
+  * 테스트
+    * 토픽생성 : docker exec -it kafka kafka-topics.sh --bootstrap-server localhost:9092 --create --topic testTopic
+    * 프로듀서 실행 : docker exec -it kafka kafka-console-producer.sh --topic testTopic --broker-list 0.0.0.0:9092
+    * 컨슈머 실행 : docker exec -it kafka kafka-console-consumer.sh --topic testTopic --bootstrap-server localhost:9092
+
+    * ApplyServiceTest
+      * Redis 초기화 
+        * docker exec -it ContainerId redis-cli
+        * flushall
+      * Topic 생성
+        -> docker exec -it kafka kafka-topics.sh --bootstrap-server localhost:9092 --create --topic coupon_create
+      * Consumer 실행 
+        -> docker exec -it kafka kafka-console-consumer.sh --topic coupon_create --bootstrap-server localhost:9092 --key-deserializer "org.apache.kafka.common.serialization.StringDeserializer" --value-deserializer "org.apache.kafka.common.serialization.LongDeserializer"
--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -1,6 +1,6 @@
 package com.example.api.service;
 
-import com.example.api.domain.Coupon;
+import com.example.api.producer.CouponCreateProducer;
 import com.example.api.repository.CouponRepository;
 import com.example.api.repository.RedisCouponRepository;
 import org.springframework.stereotype.Service;
@@ -9,19 +9,22 @@ import org.springframework.stereotype.Service;
 public class ApplyService {
     private final CouponRepository couponRepository;
     private final RedisCouponRepository redisCouponRepository;
+    private final CouponCreateProducer couponCreateProducer;
 
-    public ApplyService(CouponRepository couponRepository, RedisCouponRepository redisCouponRepository) {
+    public ApplyService(CouponRepository couponRepository, RedisCouponRepository redisCouponRepository, CouponCreateProducer couponCreateProducer) {
         this.couponRepository = couponRepository;
         this.redisCouponRepository = redisCouponRepository;
+        this.couponCreateProducer = couponCreateProducer;
     }
 
     public void apply(Long userId) {
-        long count = redisCouponRepository.increment();
+        Long count = redisCouponRepository.increment();
 
         if(count > 100) {
             return;
         }
 
-        couponRepository.save(new Coupon(userId));
+//        couponRepository.save(new Coupon(userId));
+        couponCreateProducer.create(userId);
     }
 }
